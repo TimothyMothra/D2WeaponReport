1fcc9d472cb5bd5cfdcf5bb50b3f500f3e8656d2
Timothy Mothra Lee <tilee@microsoft.com>
2021-03-08 21:00:40 -0800

    cleanup

--- src/DestinyLib.Interop/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DestinyLib.Interop
-{
-    using System.Threading.Tasks;
-
-    public class Program
-    {
-        static async Task Main(string[] arg_s) => await InitializeEnvironment.Run();
-    }
-}
---
+++ src/InitEnvironment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InitEnvironment
+{
+    using System;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static async Task Main(string[] arg_s) => await DestinyLib.Interop.InitializeEnvironment.Run();
+    }
+}
