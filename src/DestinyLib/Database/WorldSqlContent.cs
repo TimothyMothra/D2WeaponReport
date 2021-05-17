d572526ff571b3b7fe1a2c525fb75ad733a2071d
Timothy Mothra Lee <tilee@microsoft.com>
2021-05-17 16:30:52 -0700

    update

--- src/DestinyLib/Database/WorldSqlContent.cs
+++ src/DestinyLib/Database/WorldSqlContent.cs
@@ -4,10 +4,25 @@
     {
         public WorldSqlContent(string connectionString) : base(connectionString) { }
 
+        /// <summary>
+        /// This contains: Weapon definitions.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string GetDestinyInventoryItemDefinition(uint id) => GetJsonRecord("DestinyInventoryItemDefinition", id);
 
+        /// <summary>
+        /// This contains: Weapon Definition "Plugs", which are the unique collection of available perks.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string GetDestinyPlugSetDefinition(uint id) => GetJsonRecord("DestinyPlugSetDefinition", id);
 
+        /// <summary>
+        /// This contains: Weapon Stats.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public string GetDestinyStatDefinition(uint id) => GetJsonRecord("DestinyStatDefinition", id);
     }
 }
