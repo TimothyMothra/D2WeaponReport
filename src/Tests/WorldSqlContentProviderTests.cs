2d627dc14aef081ecc3cba43adec5b32066b5d35
Timothy Mothra Lee <tilee@microsoft.com>
2021-05-25 20:50:40 -0700

    Ignore Test during DEBUG

--- src/Tests/WorldSqlContentProviderTests.cs
+++ src/Tests/WorldSqlContentProviderTests.cs
@@ -90,7 +90,9 @@
         /// This test will get all weapons and attempt to parse them.
         /// If any test fails, likely something breaks the expected data schema.
         /// </summary>
-        //[Ignore] // DISABLE LOCALLY, DON'T CHECK IN. // TODO: CONVERT TO PARAM-BASED PRE-PROCESSOR https://stackoverflow.com/questions/43836548/define-c-sharp-preprocessor-from-msbuild/51782647
+#if DEBUG
+        [Ignore] // DISABLE LOCALLY. // TODO: CONVERT TO PARAM-BASED PRE-PROCESSOR https://stackoverflow.com/questions/43836548/define-c-sharp-preprocessor-from-msbuild/51782647
+#endif
         [TestMethod]
         public void TestCanParseWeapons()
         {
