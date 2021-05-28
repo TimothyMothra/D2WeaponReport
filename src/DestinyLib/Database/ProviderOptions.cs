9309b32ade41c8fa2b7c1ab67a7c4c5ac3bbb791
Timothy Mothra Lee <tilee@microsoft.com>
2021-05-28 10:04:09 -0700

    update options

--- src/DestinyLib/Database/ProviderOptions.cs
+++ src/DestinyLib/Database/ProviderOptions.cs
@@ -1,10 +1,14 @@
 ﻿namespace DestinyLib.Database
 {
-
+    /// <summary>
+    /// Options to control the behavior of Providers.
+    /// </summary>
     public class ProviderOptions
     {
-        public static ProviderOptions ScenarioDefault => new() { EnableCaching = false, };
-        
+        public static ProviderOptions ScenarioDefault => new() { EnableCaching = true, };
+
+        public static ProviderOptions TestDefault => new() { EnableCaching = false, };
+
         public static ProviderOptions TestWithCaching => new() { EnableCaching = true, };
 
         public bool EnableCaching { get; set; }
