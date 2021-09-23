533953907f2150a81154569624984287b2d46729
TimothyMothra <tilee@microsoft.com>
2021-09-22 20:01:08 -0700

    customize publish script

--- src/DestinyLib/Scenarios/InitializeEnvironmentScenario.cs
+++ src/DestinyLib/Scenarios/InitializeEnvironmentScenario.cs
@@ -1,5 +1,7 @@
 ﻿namespace DestinyLib.Scenarios
 {
+    using System;
+    using System.IO;
     using System.Threading.Tasks;
 
     using DestinyLib.Api;
@@ -9,12 +11,30 @@
     /// </summary>
     public class InitializeEnvironmentScenario
     {
-        public static async Task Run()
+        //public static async Task Run()
+        //{
+        //    Console.WriteLine("Empty Ctr");
+
+        //    var environmentDirectory = LibEnvironment.EnvironmentDirectory;
+
+        //    var manifest = new Manifest();
+        //    await manifest.DownloadWorldSqlContent(environmentDirectory);
+        //}
+
+        public static async Task Run(string[] arg)
         {
-            var environmentDirectory = LibEnvironment.EnvironmentDirectory;
+            if (arg.Length != 1)
+            {
+                throw new Exception($"Unexpected number of args: {arg.Length}");
+            }
+
+            Console.WriteLine("Test " + arg[0]);
+
+            // TODO: This value needs to come from LibEnvironment.
+            await File.Create(Path.Join(arg[0], "root.marker")).DisposeAsync();
 
             var manifest = new Manifest();
-            await manifest.DownloadWorldSqlContent(environmentDirectory);
+            await manifest.DownloadWorldSqlContent(arg[0]);
         }
 
         public static string Test()
--- src/InitEnvironment/Program.cs
+++ src/InitEnvironment/Program.cs
@@ -4,6 +4,6 @@
 
     public class Program
     {
-        public static async Task Main(string[] arg_s) => await DestinyLib.Scenarios.InitializeEnvironmentScenario.Run();
+        public static async Task Main(string[] arg_s) => await DestinyLib.Scenarios.InitializeEnvironmentScenario.Run(arg_s);
     }
 }
