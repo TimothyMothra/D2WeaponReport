a44c033568af6bed08c80e2868fda0d9c4141477
TimothyMothra <tilee@microsoft.com>
2021-10-31 11:17:18 -0700

    this combination seems to work. testing github workflow

--- src/SandboxWeb/gulpfile.js
+++ src/SandboxWeb/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='default' Clean='clean' />
+﻿/// <binding AfterBuild='gulp_copy_wwwroot' Clean='clean' />
 /*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
@@ -8,13 +8,16 @@ var del = require("del");
 var paths = {
     scripts: ["scripts/**/*.js", "scripts/**/*.map"], // "scripts/**/*.ts,"
 };
+
 gulp.task('hello', function () {
     console.log('Hello, World!, gulpfile.js');
     done();
 });
+
 gulp.task("clean", function () {
     return del(["wwwroot/scripts/**/*"]);
 });
+
 gulp.task("gulp_copy_wwwroot", function (done) {
     console.log('***gulp_copy_wwwroot!, gulpfile.js');
     gulp.src(paths.scripts).pipe(gulp.dest("wwwroot/scripts"));
