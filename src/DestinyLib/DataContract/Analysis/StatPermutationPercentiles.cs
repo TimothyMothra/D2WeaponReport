007c01a2a0f96bf5f167ecf61b860a1af7dbae11
Timothy Mothra <tilee@microsoft.com>
2021-12-08 23:07:16 -0800

    Display interpolation with the Affected Stats (#45)
    * convert to partial view, display example chart
    
    * comment
    
    * change layout
    
    * cleanup layout
    
    * print interpolation data
    
    * display charts
    
    * undo
    
    * cleanup

--- src/DestinyLib/DataContract/Analysis/StatPermutationPercentiles.cs
+++ src/DestinyLib/DataContract/Analysis/StatPermutationPercentiles.cs
@@ -1,7 +1,9 @@
 ﻿namespace DestinyLib.DataContract.Analysis
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
 
     /// <summary>
     /// An instance of <see cref="Percentiles"/> based on a set of Stat Permunations.
@@ -16,12 +18,13 @@
         /// <param name="hashId"></param>
         /// <param name="values"></param>
         /// <param name="totalCount"></param>
-        public StatPermutationPercentiles(uint hashId, string name, double baseValue, List<double> values, int totalCount)
+        public StatPermutationPercentiles(uint hashId, string name, double baseValue, List<double> values, int totalCount, IList<Tuple<double, double>> interpolationData)
         {
             this.HashId = hashId;
             this.Name = name;
             this.BaseValue = baseValue;
             this.Count = values.Count;
+            this.InterpolationData = interpolationData;
 
             // Note: not all perk permutations will affect each stat.
             // Need to pad the stat Permutations here with 0's so the percentiles are correct.
@@ -42,6 +45,24 @@
         public int Count { get; private set; }
 
         public Percentiles Percentiles { get; private set; }
+
+        public IList<Tuple<double, double>> InterpolationData { get; set; }
+
+        /// <summary>
+        /// <code>[{ x: 0, y: 20}, { x: 10, y: 10}, { x: 20, y: 15}, { x: 30, y: 10}]</code>.
+        /// </summary>
+        /// <returns></returns>
+        public string GetInterpolationDataAsString()
+        {
+            if (this.InterpolationData == null)
+            {
+                return "null";
+            }
+
+            var list = string.Join(",", this.InterpolationData.Select(x => $"{{x: {x.Item1}, y: {x.Item2}}}"));
+
+            return $"[{list}]";
+        }
 
         public override string ToString()
         {
